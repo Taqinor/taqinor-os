13c8bea1d8a6eaa9e2bdd8da60fb4035049935b5
Reda Kasri <kasri.reda@gmail.com>
2026-07-03 06:48:02 +0100

    fix(migr): rechain customfields stock custom_data 0029->0040 after stock chain (collision)

--- backend/django_core/apps/stock/migrations/0040_fournisseur_custom_data.py
+++ backend/django_core/apps/stock/migrations/0040_fournisseur_custom_data.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("stock", "0028_dc34_fournisseur_type_soustraitantprofile"),
+        ("stock", "0039_xmfg11_rebut"),
     ]
 
     operations = [
